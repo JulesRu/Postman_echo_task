45c0e889be4354f674522602393460bf81e9c272
JuliaR <donidonic@gmail.com>
2023-04-01 17:36:38 +0300

    Test with correct data path

--- src/test/java/PostTest.java
+++ src/test/java/PostTest.java
@@ -19,7 +19,7 @@ public class PostTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("До свидания!"))
+                .body("data", equalTo("Здравствуйте!"))
         ;
     }
 }
